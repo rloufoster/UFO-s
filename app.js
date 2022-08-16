d834d44fa891b1e4621e376ebe4584a58dadc1bc
Robin Foster <robinlfoster@att.net>
2022-08-16 16:56:34 -0500

    Uploading inital data and code from module excersises

---
+++ app.js
@@ -0,0 +1,9 @@
+
+// import the data from data.js
+
+const tableData = data;
+
+// Reference the HTML table using d3
+// This step declares variable, tbody and tells JavaScript to look for the <tbody> tags in HTML
+
+var tbody = d3.select("tbody");

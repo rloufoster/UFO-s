bf4d3b31b4d6371884c92d0fc30f3edff4db0180
Robin Foster <robinlfoster@att.net>
2022-08-21 10:03:20 -0500

    Uploading working challenge code

--- app.js
+++
@@ -1,9 +0,0 @@
-
-// import the data from data.js
-
-const tableData = data;
-
-// Reference the HTML table using d3
-// This step declares variable, tbody and tells JavaScript to look for the <tbody> tags in HTML
-
-var tbody = d3.select("tbody");
